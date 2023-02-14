fa3fab6513b191b223ce44aafabe2ccef94cbeca
jyrnan <yoeking@gmail.com>
2023-02-14 11:13:50 +0800

    Initial Commit

---
+++ YiTVLinkSDKDemoAPP/YiTVLinkSDKDemoAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YiTVLinkSDKDemoAPPApp.swift
+//  YiTVLinkSDKDemoAPP
+//
+//  Created by jyrnan on 2023/2/14.
+//
+
+import SwiftUI
+
+@main
+struct YiTVLinkSDKDemoAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
